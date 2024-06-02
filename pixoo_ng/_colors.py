e5b1d38f08d6c0864732eb85d960551033c35f2e
crish <crish@mailbox.org>
2024-06-02 13:58:57 +0200

    Add more colors.

--- pixoo_ng/_colors.py
+++ pixoo_ng/_colors.py
@@ -2,12 +2,18 @@
 
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
+COLOR_GREEN = (99,199, 77)
+COLOR_RED   = (255, 0, 68)
+COLOR_GRAY  = (57, 57, 57)
 
 
 class Palette:
     """Class representing standard colors"""
     BLACK = COLOR_BLACK
     WHITE = COLOR_WHITE
+    GREEN = COLOR_GREEN
+    RED   = COLOR_RED
+    GRAY  = COLOR_GRAY
 
 
-__all__ = ["Palette", "COLOR_BLACK", "COLOR_WHITE"]
+__all__ = ["Palette", "COLOR_BLACK", "COLOR_WHITE", "COLOR_GREEN", "COLOR_RED"]
